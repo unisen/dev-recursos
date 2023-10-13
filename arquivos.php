23258195ea004cbe6747ac084456782c9a51c905
unisen <unisen.website@gmail.com>
2023-10-13 09:16:12 -0300

    add arquivos.php in root

---
+++ arquivos.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html lang>
+
+<head>
+  <title>LISTA DE ARQUIVOS</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="/dashboard/fontawesome-6.4.0/css/all.css">
+
+   <!-- support v4 icon references/syntax -->
+   <link href="/dashboard/fontawesome-6.2.1/css/v4-shims.css" rel="stylesheet">
+
+   <style>
+    .nav-item .nav-link-custom,
+    .navbar-light .navbar-brand {
+        color: #fff !important;
+    }
+
+  </style>
+</head>
+
+<body>
+  <!--[if lt IE 7]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+  
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" style="background-color: #000000 !important;">
+      <a class="navbar-brand" href="/dashboard/">&larr; Voltar</a>
+      <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+          aria-expanded="false" aria-label="Toggle navigation"></button>
+      <div class="collapse navbar-collapse" id="collapsibleNavId">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a class="nav-link nav-link-custom" href="/index.php">Dashboard <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link nav-link-custom" href="/index-pastas.php">Index Pastas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link nav-link-custom" href="/arquivos.php">Arquivos </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:chartreuse">ONLINE</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownId">
+              <a class="dropdown-item" href="https://unisen.com.br/site/" target="_blank">UNISEN - SITE</a>
+              <a class="dropdown-item" href="https://goo.gl/maps/kVhXArKQqh9vH2zK9" target="_blank">UNISEN - GBusiness Map</a>
+              <a class="dropdown-item" href="https://www.google.com/search?hl=pt-BR&authuser=0&sxsrf=APwXEdepy7BHvxgAfBCQAzTVQZjRLpnE9A%3A1683159607104&q=Unisen%20-%20Escalas%20M%C3%A9dicas&stick=H4sIAAAAAAAAAONgU1IxqLA0Nk1NMzRJTU5OMbVIMTW3MqgwTEo2MzBOM0i1TE41SrFYxCoZmpdZnJqnoKvgWpycmJNYrOB7eGVKZnJiMQA0fRgkRAAAAA&mat=Cd00QoMYnhkI&ved=2ahUKEwiA8uyTstr-AhXeqpUCHTIQAiwQrMcEegQIBRAD" target="_blank">UNISEN - Google MEU NEGOCIO</a>
+              <a class="dropdown-item" href="https://www.unisen.com.br/cadastro/cadastro.php" target="_blank">UNISEN - Cadastro Sistema</a>
+              <a class="dropdown-item" href="https://unisen.com.br/adm/cadastro.php" target="_blank">UNISEN - ADM Cadastro</a>
+              <a class="dropdown-item" href="https://unisen.com.br/sen/login.php" target="_blank">UNISEN - SEN Login</a>
+              <a class="dropdown-item" href="https://unisen.com.br/sen/cadastro.php" target="_blank">UNISEN - SEN Cadastro</a>             
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:red">LOCAL</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownId">
+              <a class="dropdown-item" href="/unisen/app/cadastro/cadastro.php" target="_blank">UNISEN - Cadastro Sistema</a>
+              <a class="dropdown-item" href="/unisen/app/adm/cadastro.php" target="_blank">UNISEN - ADM Cadastro</a>
+              <a class="dropdown-item" href="/unisen/app/sen/login.php?page=../cadastro/atualizadocs.php&u=hap" target="_blank">UNISEN - SEN Login</a>
+              <a class="dropdown-item" href="/unisen/app/sen/cadastro.php" target="_blank">UNISEN - SEN Cadastro</a>       
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:#1296ab;font-weight:600">TEMPLATES ADMIN</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownId">
+              <a class="dropdown-item" href="/templates/light-bootstrap-dashboard-master/examples/dashboard.html" target="_blank">Light Bootstrap Dashboard</a>
+              <a class="dropdown-item" href="/templates/argon-dashboard-master/" target="_blank">Argon Dashboard</a>
+              <a class="dropdown-item" href="/templates/black-dashboard-master/examples/dashboard.html" target="_blank">Black Dashboard</a>
+              <a class="dropdown-item" href="/templates/corporate-ui-dashboard-main/pages/dashboard.html" target="_blank">Corporate UI Dashboard</a>
+              <a class="dropdown-item" href="/templates/material-dashboard-master/pages/dashboard.html" target="_blank">Material Dashboard</a>   
+              <a class="dropdown-item" href="/templates/now-ui-dashboard-master/examples/dashboard.html" target="_blank">Now UI Dashboard</a>   
+              <a class="dropdown-item" href="/templates/paper-dashboard-master/examples/dashboard.html" target="_blank">Paper Dashboard</a>   
+              <a class="dropdown-item" href="/templates/iconic-admin-dashboard/html/dist/index.html" target="_blank">Iconic Admin Dashboard</a>     
+              <a class="dropdown-item" href="/templates/droopy_themeforest/">Droopy Admin Dashboard</a>
+              <a class="dropdown-item" href="/templates/flatkit-dashboard-ui-kit/html/dashboard.html" target="_blank">Flatkit UI Kit Dashboard</a> 
+              <a class="dropdown-item" href="/templates/Dastyle-v1.1/dastyle_v1.1/" target="_blank">Dastyle V 1.1</a> 
+              <a class="dropdown-item" href="/templates/aside-admin-dashboard-ui-kit/html/dashboard.html" target="_blank">Aside Admin Dashboard</a> 
+              <a class="dropdown-item" href="/templates/flow-1.2.2/dist/" target="_blank">Flow Pro Dashboard</a> 
+              <a class="dropdown-item" href="/templates/ultra-admin-package/Bootstrap 4/Ultra admin HTML -BS4/" target="_blank">Ultra Admin Dashboard</a> 
+              <a class="dropdown-item" href="/templates/compass-bootstrap-4-multipurpose-admin-template/html/" target="_blank">Compass Bootstrap 4</a> 
+              <a class="dropdown-item" href="/templates/compass-bootstrap-4-multipurpose-admin-template/hospital/" target="_blank">Compass Hospital</a> 
+              <a class="dropdown-item" href="/templates/compass-bootstrap-4-multipurpose-admin-template/estate/" target="_blank">Compass Estate</a> 
+              <a class="dropdown-item" href="/templates/StarAdmin/" target="_blank">StarAdmin</a> 
+              <a class="dropdown-item" href="/templates/star-admin2-v.1.0/template/" target="_blank">StarAdmin 2</a> 
+              <a class="dropdown-item" href="/templates/mooli-hospital/hospital/html/" target="_blank">Mooli Hospital</a>    
+            </div>
+          </li>          
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+</br>
+<br><br>
+<h1 style="text-align: center;">LISTA DE ARQUIVOS e PASTAS</h1>
+  <hr>
+<div class="container">
+  
+<table class="table">
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Tipo</th>
+        <th>Data de Criação</th>
+        <th>Data de Modificação</th>
+        <th>Comando</th>
+      </tr>
+    </thead>
+    <tbody>
+    
+    <?php
+    $pasta_atual = basename(__DIR__);
+    $path = "/xampp//" . $pasta_atual . "/";
+    $diretorio = dir($path);
+    $localhost_url = "/"; // . $pasta_atual . "/";
+
+
+    echo "Lista de Arquivos do diretório <strong>/" . $pasta_atual . "</strong>:<br />";
+    while($arquivo = $diretorio -> read()){
+
+      echo '<tr>'; 
+
+      if (is_file($arquivo)) {
+        
+        echo "<td><a href='".$arquivo."'>".$arquivo."</a></td>";
+
+        echo '<td>Arquivo</td>';
+     
+        $date_create = date ("d/m/Y H:i:s", filectime($arquivo));
+        // imprime somente a parte de índices associativos
+        echo '<td>' . $date_create . '</td>';
+
+        $date_mod = date ("d/m/Y H:i:s", filemtime($arquivo));
+        // imprime somente a parte de índices associativos
+        echo '<td>' . $date_mod . '</td>';
+
+        echo '<td style="text-align: center;"><a href=' . $localhost_url . $arquivo . ' target="_blank"> <i class="fa-solid fa-right-to-bracket"> </i></a></td>';
+
+        echo '</tr>';
+      
+      }
+      else {
+        echo "<td><a href='".$arquivo."' style='text-transform: uppercase; font-weight: bold;'>".$arquivo."</a></td>";
+        echo '<td><b>Diretório</b></td>';
+
+        $date_create = date ("d/m/Y H:i:s", filectime($arquivo));
+        // imprime somente a parte de índices associativos
+        echo '<td>' . $date_create . '</td>';
+
+        $date_mod = date ("d/m/Y H:i:s", filemtime($arquivo));
+        // imprime somente a parte de índices associativos
+        echo '<td>' . $date_mod . '</td>';
+
+        echo '<td style="text-align: center;"><a href=' . $localhost_url . $arquivo . ' target="_blank"> <i class="fa-solid fa-right-to-bracket"> </i></a></td>';
+        echo '</tr>';
+     
+      }
+    }
+    $diretorio -> close();
+
+    ?>
+<br/><hr>
+
+<?php
+
+
+// abre um arquivo
+//$fp = fopen("/vendderapp/", "r");
+
+// colhe as estatísticas
+//$fstat = fstat($fp);
+
+// fecha o arquivo
+//fclose($fp);
+
+// imprime somente a parte de índices associativos
+//print_r(array_slice($fstat, 13));
+
+?>
+
+<br/><hr>
+
+</div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
